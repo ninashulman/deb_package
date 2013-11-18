b1a630607dfe8d8c20a092fafdf740c464d42215
Nina Shulman <ninkin@yahoo.com>
2013-11-18 00:15:33 -0800

    Added more error handling

--- builddeb.py
+++ builddeb.py
@@ -2,70 +2,80 @@ import sys
 import os
 
 packageName = 'stackstorm-capacity'
-ver = '-1.0'
-buildNum = 1
-debDirName = packageName + ver
+version = '-1.0'
+packageVerName = packageName + version
+sourceDir = './usr/share/' + packageName
+debDirName = packageName + version
 debFileName = debDirName + '.deb'
 
+workDir = os.getcwd()
+
+def cleanUp(msg):
+    os.chdir(workDir)
+    if os.path.exists('debtmp'):
+        os.system('rm -rf debtmp')
+    print msg
+    sys.exit()
 
 
-# Create tmp directory for temprorary files which will be deleted later
-if not os.path.exists('tmp'):
-	os.makedirs('tmp')
-os.chdir('tmp')
+# Create debtmp directory for temprorary files which will be deleted later
+if os.path.exists('debtmp'):
+    os.system('rm -rf debtmp')
+
+os.makedirs('debtmp')
+os.chdir('debtmp')
 
 # Clone the content of https://github.com/ninashulman/deb_package into stackstorm-capacity-1.0/
-if not os.path.exists('stackstorm-capacity-1.0'):
-	os.system('git clone https://github.com/ninashulman/deb_package stackstorm-capacity-1.0 -b master')
-else:
-	print "Remove or rename tmp/stackstorm-capacity-1.0/ in order to proceed with the build process"
-	sys.exit()
+if os.system('git clone https://github.com/ninashulman/deb_package ' + packageVerName + ' -b master'):
+        cleanUp('Failed to clone debian files. Cannot proceed building package.')
 
 # Create directory for source code
-os.chdir('./stackstorm-capacity-1.0')
-source_dir = './usr/share/stackstorm-capacity'
-os.makedirs(source_dir)
-print 'Source_dir created = %s' % source_dir
-os.chdir(source_dir)
+os.chdir('./'+ packageVerName)
+os.makedirs(sourceDir)
+print 'Created %s directory for StackStorm source code' % sourceDir
+os.chdir(sourceDir)
 
 # Clone source into stackstorm-capacity-1.0/usr/share/stackstorm-capacity/
-os.system('git clone https://github.com/StackStorm/osh -b master')
-os.system('git clone https://github.com/StackStorm/osh-data -b master')
-os.system('git clone https://github.com/StackStorm/osh-ui -b master')
+# os.system returns 0 if the action completed successfully and error code (> 0) if action failed
+if (os.system('git clone https://github.com/StackStorm/osh -b master') or
+    os.system('git clone https://github.com/StackStorm/osh-data -b master') or
+    os.system('git clone https://github.com/StackStorm/osh-ui -b master')
+    ):
+    # if os.system returs error code, then if statatemnt is true
+    cleanUp('Failed to clone StockStorm source code. Cannot proceed building package.')
+    
 
 # Build a source package stackstorm-capacity_1.0-1.dsc
 os.chdir('../../../')
-res = os.system('debuild --no-tgz-check -S')
-#   click y for yes when prompted   
-if res != 0:
-	print 'Building source package failed'
-	sys.exit(1)
+if os.system('debuild --no-tgz-check -S'):
+    # if os.system returs error code, then if statatemnt is true
+    cleanUp('Building source package failed')
 
-# Build a binary debian package in the current directory
+# Build a binary debian package
 os.chdir('../')
-res = os.system('dpkg-deb -b ' + debDirName + ' .')
-if res != 0:
-	print 'Building binary .deb package failed'
-	sys.exit(1)
+if os.system('dpkg-deb -b ' + debDirName + ' .'):
+    # if os.system returs error code, then if statatemnt is true
+    cleanUp('Building binary .deb package failed')
 
-# Storing deb package and cleaning
+# Store Debian package in the outside directory ../artifacts/debPackages 
 files = os.listdir('.')
-print 'List of generated files: ' + str (files)
+# print 'List of generated files: ' + str (files)
 for fileName in files:
     if fileName.endswith(".deb"):
         if os.path.exists('../artifacts/debPackages'):
-        	oldFileNames = os.listdir('../artifacts/debPackages')
-        	if fileName in oldFileNames:
-        		os.system('mv ../artifacts/debPackages/' + fileName + ' ../artifacts/debPackages/' + fileName + '-old')
-        		print 'Previously built ' + fileName + ' is renamed into ' + fileName + '-old'
+            oldFileNames = os.listdir('../artifacts/debPackages')
+            if fileName in oldFileNames:
+                os.system('mv ../artifacts/debPackages/' + fileName + ' ../artifacts/debPackages/' + fileName + '-old')
+                print 'Previously built ' + fileName + ' is renamed into ' + fileName + '-old'
         else:
-        	os.makedirs('../artifacts/debPackages')  	
-        os.system('mv %s ../artifacts/debPackages' % fileName)
-print 'Debian package is created successfully and stored in ./artifacts/debPackages directory'
+            os.makedirs('../artifacts/debPackages')
+  
+        if os.system('mv %s ../artifacts/debPackages' % fileName):
+            # if os.system returs error code, then if statatemnt is true
+            cleanUp('Storing binary .deb package failed')
+        
+        print 'Debian package is created successfully and stored in ./artifacts/debPackages directory'
 
-# Removing tmp directory'
-print 'Removing tmp directory to clean up'
+# Removing debtmp directory'
 os.chdir('../')
-os.system('rm -rf tmp')
-
-
+os.system('rm -rf debtmp')
