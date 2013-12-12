01a6637e680517b4173b559160224c491d7111a2
Nina Shulman <ninkin@yahoo.com>
2013-12-12 00:13:02 -0800

    Added command line options

--- README.md
+++ README.md
@@ -1,111 +1,16 @@
-# ####################
-# How to set GPGKEY if you do not have it (needed for signing deb packages)
-# ####################
-# Step 0
-$ sudo apt-get install pgp gnupg-agent 
+Usage:  python builddeb.py [<option> ...]
 
-# Step 1
-# To help generating GPG key, get ready to run in a separace shell (on the same machine)
-$ find / &gt; /dev/null   
-# run it while you are generating gpgkey in the next step and kill this process after GPG key is generated
+Purpose: Build stackstorm-capacity-1.0_amd64.deb package for 64 bit Ubuntu.
+	       If no additional commands or options are specified, the package is built 
+	       from the github master branch source and stored locally in the
+	       ./artifacts/debPackages directory, while all intermediate files are deleted.
 
-# Generate gpgkey
-$ gpg --gen-key --no-use-agent 
-# option 4 ( RSA (sign only), use defaults)
-# Observe your gpgkey being generated and save its value
-# Example:  key 6A25F155 marked as ultimately trusted
-
-# Step 2
-# check that your genkey is listed
-$ gpg --list-secret-keys
-# Example:
-# /home/ninas/.gnupg/secring.gpg
-# -----------------------------
-# sec   2048R/6A25F155 2013-10-29
-# uid     Nina Shulman <ninas@saasbook.com>
-
-# Step 3
-# Add your gpgkey to ~/.bashrc file
-# Example of line in  ~/.bashrc file:  export GPGKEY=6A25F155
-
-# Step 4
-#Now restart the gpg-agent and source your .bashrc again:	
-$ killall -q gpg-agent
-$ eval $(gpg-agent --daemon)
-$ source ~/.bashrc
-
-# Step 5
-# check that keys set correctly to the value your exported in ~/.bashrc file
-$ echo $GPGKEY
-
-# ####################
-# How to build stackstorm-capacity debian package for Ubuntu
-# ####################
-# Step 0
-$ sudo apt-get install devscripts
-
-# Step 1 
-# Clone the content of https://github.com/ninashulman/deb_package into package_name-1.0/
-$ git clone https://github.com/ninashulman/deb_package package_name-1.0
-# Cloned package_name-1.0 directory consists of debian\ and DEBIAN\ which were produced by running
-#               dh_make -e youremail@address -f ../package_name-1.0.orig.tar.gz
-#               and altered to fit the info needed for future building and installation of the package
-
-# Step 2
-# Create directory for source code inside of package_name-1.0/
-$ cd package_name-1.0/
-$ mkdir -p usr/share/package_name
-
-# Step 3
-# Unzip the source directory content into package_name-1.0/usr/share/package_name/
-
-# Step 4
-# Update the Architecture line in the debian/control file to match the one used on the client machine
-# If in doubt, on the client machine run $ uname -i  
-# Examples of the line to include in the control file: 
-#       Architecture: i386
-#       Architecture: amd64
-
-# Step 5
-# Check that you have usr/, debian/ , DEBIAN/, and README.md , inside package_name-1.0/
-
-# Step 6
-# Build a source package package_name_1.0-1.dsc
-$ cd package_name-1.0/
-$ debuild -S
-#   click y for yes when prompted   
-
-# Step 7
-# Go outside of_name-1.0/ and observe package_name_1.0-1.dsc being created there 
-$ cd ..
-
-# Step 8
-# Build a binary debian package package_name_1.0-1_i386.deb in the current directory
-$ dpkg-deb -b package_name-1.0 .
-
-# Done! package_name_1.0-1_i386.deb is ready to be used  for installation on Ubuntu machine
-# You may rup dpkg -I (with capital I) to see info about this package: $ dpkg -I  package_name_1.0-1_i386.deb
-
-# ####################
-# How to install a debian package on Ubuntu machine
-# ####################
-
-# Step 1
-# Run $ uname -i to make sure that your package was built for your hardware platform architecture
-#                   For i386, use package_name_1.0-1_i386.deb
-#                   For amd64, use package_name_1.0-1_amd64.deb
-
-
-# Step 2
-# Install the package onto the client machine
-$ sudo dpkg -i  package_name_1.0-1_i386.deb
-
-# Step 3
-# How to check where the package was installed and more info if needed
- $ dpkg -L package_name
- $ dpkg --status  package_name
- $ dpkg -I package_name_version.deb  #that -I must be capital
-
-# Done! Package is installed
-# Package can be easily uninstalled by running $ sudo apt-get remove package_name
-
+Options:
+  -s <src_dir>		  Build package from the source in the specified directory insted of source in github.
+  -p  ...           Install all prerequisites documented for this package.
+  -w  ...           DO NOT upload, unzip and include Solr (open source search platform) into this package.
+  -b <branch_name>  Clone from the specified branch (master by default).
+  -m  ...		        DO NOT clean up all intermediate files collected in debtmp directory.
+  -d  ...		        Display package description after it is built.
+  -i  ...		        Install package after it is built.
+  -h  ...		        Show this help message.
--- builddeb.py
+++ builddeb.py
@@ -23,9 +23,9 @@ def cleanUp(msg):
 def cloneGithubSource(branch):
     # Clone source into stackstorm-capacity-1.0/usr/share/stackstorm-capacity/
     # os.system returns 0 if the action completed successfully and error code (> 0) if action failed
-    if (os.system('git clone https://github.com/StackStorm/osh -b ' + branch) or
-        os.system('git clone https://github.com/StackStorm/osh-data -b ' + branch) or
-        os.system('git clone https://github.com/StackStorm/osh-ui -b ' + branch)
+    if (os.system('git clone https://github.com/StackStorm/osh -b %s' % branch) or
+        os.system('git clone https://github.com/StackStorm/osh-data -b %s' % branch) or
+        os.system('git clone https://github.com/StackStorm/osh-ui -b %s' % branch)
         ):
         # if os.system returs error code, then if statatemnt is true
         cleanUp('Failed to clone StockStorm source code. Cannot proceed building package.')    
@@ -39,13 +39,13 @@ def build(args):
    
     # Create a temp directory for build and source files
     if os.path.exists(tmpDir):
-        os.system('rm -rf ' + tmpDir)
+        os.system('rm -rf %s' % tmpDir)
 
     os.makedirs(tmpDir)
     os.chdir(tmpDir)
     
     # Clone the content of https://github.com/ninashulman/deb_package into stackstorm-capacity-1.0/
-    if os.system('git clone https://github.com/ninashulman/deb_package ' + packageVerName + ' -b master'):
+    if os.system('git clone https://github.com/ninashulman/deb_package %s -b master' % packageVerName):
             cleanUp('Failed to clone debian files. Cannot proceed building package.')
 
     # Create directory for source code
@@ -55,8 +55,8 @@ def build(args):
     os.chdir(sourceDir)
 
     # Get source code from github or specified path
-    if "-d"  in args:
-        i = args.index("-d")
+    if "-s"  in args:
+        i = args.index("-s")
         src_dir = args[i+1]   
         os.system('cp -rf %s/* .' % src_dir)
     elif "-b" in args:
@@ -71,13 +71,12 @@ def build(args):
         # Download and unzip solr
         if (os.system('wget http://mirrors.sonic.net/apache/lucene/solr/4.6.0/solr-4.6.0.tgz') or
             os.system('tar -xvpf solr-4.6.0.tgz') or
-            os.system('rm solr-4.6.0.tgz')
+            os.system('rm -f solr-4.6.0.tgz')
             ):
             # if os.system returs error code, then if statatemnt is true
             cleanUp('Failed to download and unzip solr used by StackStorm. Discontinue the building process.')
     
-    os.chdir(workDir + '/' + tmpDir +'/' + packageVerName)
-    print os.getcwd()   #ns
+    os.chdir('%s/%s/%s' % (workDir, tmpDir, packageVerName))
 
     if os.path.exists(tmpDir):
         os.chdir(tmpDir)
@@ -90,36 +89,35 @@ def build(args):
 
     # Build a binary debian package
     os.chdir('../')
-    if os.system('dpkg-deb -b ' + packageVerName + ' .'):
+    if os.system('dpkg-deb -b %s .' % packageVerName):
         # if os.system returs error code, then if statatemnt is true
         cleanUp('Building binary .deb package failed')
 
     # Store Debian package in the outside directory ../artifacts/debPackages 
     files = os.listdir('.')
-    # print 'List of generated files: ' + str (files)
     for fileName in files:
         if fileName.endswith(".deb"):
-            if os.path.exists('../artifacts/debPackages'):
-                oldFileNames = os.listdir('../artifacts/debPackages')
+            if os.path.exists(packageDir):
+                oldFileNames = os.listdir(packageDir)
                 if fileName in oldFileNames:
-                    os.system('mv ../artifacts/debPackages/' + fileName + ' ../artifacts/debPackages/' + fileName + '-old')
-                    print 'Previously built ' + fileName + ' is renamed into ' + fileName + '-old'
+                    os.system('mv %s%s %s%s-old' % (packageDir, fileName, packageDir, fileName))
+                    print 'Previously built %s is renamed into %s-old' % (fileName, fileName)
             else:
-                os.makedirs('../artifacts/debPackages')
+                os.makedirs(packageDir)
       
-            if os.system('mv %s ../artifacts/debPackages' % fileName):
+            if os.system('mv %s %s' % (fileName, packageDir)):
                 # if os.system returs error code, then if statatemnt is true
                 cleanUp('Storing binary .deb package failed')
             
             packageFileName = fileName
-            print 'Debian package is created successfully and stored in ./artifacts/debPackages directory'
+            print 'Debian package is created successfully and stored in %s directory' % packageDir
 
     # Removing build directory'
     os.chdir('../')
     if not "-m" in args:
         os.system('rm -rf ' + tmpDir)
 
-    if "-i" in args:
+    if "-d" in args:
         os.system('dpkg -I %s%s' % (packageDir, packageFileName))
 
     if "-p" in args:
@@ -128,23 +126,28 @@ def build(args):
         os.system('./deploy/install-prereqs.sh')
         os.system('rm -rf deploy')
 
+    if "-i" in args:
+        print "Installing the package after it is built"
+        os.system('sudo dpkg -i %s%s' % (packageDir, packageFileName))   
+
 def usage():
     print
-    print "Usage:  python %s [<command> ...] [<option> ...]" % os.path.basename(sys.argv[0])
+    print "Usage:  python %s [<option> ...]" % os.path.basename(sys.argv[0])
     print
-    print "Purpose: Build "+ packageVerName + "_amd64.deb" +  " package for 64 bit Ubuntu."
-    print "\t If no additional commands or options are specified, the package is built from the github master branch source and "
-    print "\t stored locally in the ./artifacts/debPackages directory, while all intermediate files are deleted."
+    print "Purpose: Build %s_amd64.deb package for 64 bit Ubuntu." % packageVerName
+    print "\t If no additional commands or options are specified, the package is built "
+    print "\t from the github master branch source and stored locally in the"
+    print "\t %s directory, while all intermediate files are deleted." % packageDir
     print 
-    print "Commands:"  
-    print "  -d <src_dir>\t\tBuild package from the source in the specified directory insted of source in github"
-    print "  -p  ...\t\tInstall all prerequisites documented for this package"
-    print "  -w  ...\t\tDO NOT upload, unzip and include Solr (open source search platform) into this package"
-    print
     print "Options:"
-    print "  -b <branch_name>\tClone from the specified branch (master by default)"
-    print "  -m  ...\t\tDO NOT clean up all intermediate files collected in " + tmpDir + " directory"   
-    print "  -i  ...\t\tDisplay package info after it is built"
+    print "  -s <src_dir>\t\tBuild package from the source in the specified directory insted of source in github."
+    print "  -p  ...\t\tInstall all prerequisites documented for this package."
+    print "  -w  ...\t\tDO NOT upload, unzip and include Solr (open source search platform) into this package."
+    print "  -b <branch_name>\tClone from the specified branch (master by default)."
+    print "  -m  ...\t\tDO NOT clean up all intermediate files collected in %s directory." % tmpDir 
+    print "  -d  ...\t\tDisplay package description after it is built."
+    print "  -i  ...\t\tInstall package after it is built."
+    print "  -h  ...\t\tShow this help message."
 
 def main(argv=None):
     if argv is None:
